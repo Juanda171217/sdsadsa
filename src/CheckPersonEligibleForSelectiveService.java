3a09c5f0f2f725ce904bd9a31619c182dfdd4642
Juan David <juandamarcar@hotmail.com>
2023-02-10 15:44:03 -0500

    d

---
+++ src/CheckPersonEligibleForSelectiveService.java
@@ -0,0 +1,7 @@
+public class CheckPersonEligibleForSelectiveService implements CheckPerson {
+    public boolean test(Person p) {
+        return p.gender == Person.Sex.FEMALE &&
+                p.getAge() >= 18 &&
+                p.getAge() <= 25;
+    }
+}
